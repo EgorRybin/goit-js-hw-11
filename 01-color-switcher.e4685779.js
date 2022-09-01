46d028dc16939d2b2debeac27bd724e627761243
EgorRybin <107394961+EgorRybin@users.noreply.github.com>
2022-09-01 13:25:51 +0000

    Deploying to gh-pages from @ EgorRybin/goit-js-hw-11@452a1d3038e3df1af1c12b07e5a5cbda084a3cd6 🚀

---
+++ 01-color-switcher.e4685779.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("button"),n=e.nextElementSibling,t=document.querySelector("body"),d=null;e.disabled=!1,n.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,d=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.e4685779.js.map
