69bcdd7fa48d41122eb11771f28118b12baa12ce
EgorRybin <antoniopareiro@gmail.com>
2022-09-01 18:49:37 +0300

    update js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const API_KEY = '29634841-061e0c7ab4009b86045ba35d0';
 const URL = 'https://pixabay.com/api/?key=';
@@ -12,27 +13,54 @@ refs = {
   loadMoreBtn: document.querySelector('.load-more'),
 };
 
+let pageNumber = 1;
+let totalPages = 0;
+
+
 refs.form.addEventListener('submit', onSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 async function onSubmit(evt) {
   evt.preventDefault();
+  refs.loadMoreBtn.classList.remove('is-hidden');
   const inputText = refs.input.value;
 
   try {
     const data = await getFoto(inputText);
     const markup = markupGallery(data.hits);
+      if (markup.length <= 0) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
     renderGallery(markup);
   } catch (error) {
     console.log('error', error);
   }
-};
-
+}
+ 
+ async function onLoadMore() {
+   pageNumber += 1;  
+   const { data } = await axios.get(
+     `${URL}${API_KEY}&q=${refs.input.value}&page=${pageNumber}&image_type=photo&per_page=40&orientation=horizontal&safesearch=true`
+   );
+ let totalPages = data.totalHits;
+   if (totalPages < (pageNumber + 1) * 100) {
+     refs.loadMoreBtn.classList.add('is-hidden');
+     Notiflix.Notify.failure(
+       "We're sorry, but you've reached the end of search results."
+     );
+   }
+   const markup = markupGallery(data.hits);
+   refs.gallery.insertAdjacentHTML('beforeend', markup);
+ }
 async function getFoto(inputText) {
   const { data } = await axios.get(
-    `${URL}${API_KEY}&q=${inputText}&image_type=photo&orientation=horizontal&safesearch=true`
+    `${URL}${API_KEY}&q=${inputText}&image_type=photo&per_page=40&orientation=horizontal&safesearch=true`
   );
   return data;
-};
+}
 
 function markupGallery(array) {
   const markup = array
@@ -56,18 +84,9 @@ function markupGallery(array) {
   </div>`
     )
     .join('');
-  console.log(markup);
   return markup;
-};
+}
 
 function renderGallery(markup) {
-refs.gallery.innerHTML = markup;
+  refs.gallery.innerHTML = markup;
 }
-// function getFoto(inputText) {
-//   fetch(
-//     `${URL}${API_KEY}&q=${inputText}&image_type=photo&orientation=horizontal&safesearch=true`
-//   )
-//     .then(res => res.json())
-//     .then(data => console.log(data.hits))
-//     .catch(err => console.log(err));
-// }
